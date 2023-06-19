906112e97235b496240ef85b573b9513ac131909
Stefano Morelli <stefanomorelli95@gmail.com>
2023-06-19 23:27:58 +0200

    Card and dataDriven Test

--- src/component/about.js
+++ src/component/about.js
@@ -5,7 +5,8 @@ export default function About(){
         <div className="card-about">
             <h1>About</h1>
             <h3>Lorem Ipsum the idea of the xxx over the
-            space X, conquista dello spazio targata america, Lorem Ipsum the idea of the xxx over the space X, conquista dello spazio targata america</h3>
+            space X, conquista dello spazio targata america, Lorem Ipsum the idea of the xxx over the space X, conquista dello spazio targata america facciamo qualcosa
+            </h3>
         </div>
     )
 }
